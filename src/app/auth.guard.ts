038b74d3d4397c468f0f5508e96c0dea3a2087ad
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-03-19 09:35:47 +0100

    Pokedex en PROD

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {  CanActivate, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(){
-    console.log('Le guard a bien été appelé !')
-    return true;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ){}
+
+  canActivate(): boolean{
+    if(this.authService.isLoggedIn){
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
   }
-  
 }
